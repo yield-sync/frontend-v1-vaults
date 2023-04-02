b48fbbb574f29fd106534c8c5f656355f49a4c30
aleem ahmed <w3st.io2021@gmail.com>
2023-04-01 20:45:12 -0400

    Read on goerli

---
+++ src/variables.tsx
@@ -0,0 +1,11 @@
+export default {
+	address: {
+		mainnet: {
+			yieldSyncV1VaultFactory: ""
+		},
+
+		goerli: {
+			yieldSyncV1VaultFactory: "0xEa028EC5038d71468c097D009c894f138e0191B1"
+		},
+	}
+};
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,28 @@
 <template>
 	<VContainer>
-		<VRow/>
+		<VRow>
+			contract
+		</VRow>
 	</VContainer>
 </template>
+
+<script lang="ts">
+	import { defineComponent } from "vue";
+	import { AbiItem } from "web3-utils";
+	import YieldSyncV1VaultFactory from "../abi/YieldSyncV1VaultFactory";
+	import v from "../variables";
+
+	export default defineComponent({
+		name: "BalancesView",
+
+		async created() 
+		{
+			const contract = new this.$store.state.web3.eth.Contract(
+				YieldSyncV1VaultFactory as AbiItem[],
+				v.address.goerli.yieldSyncV1VaultFactory
+			);
+
+			console.log(await contract.methods.YieldSyncV1VaultRecord().call());
+		}
+	});
+</script>
