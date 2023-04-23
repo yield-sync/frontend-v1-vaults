560c15fe78395678e2973336482dbbe00d764498
aleem ahmed <w3st.io2021@gmail.com>
2023-04-23 00:15:40 -0400

    Setting up for tabs

--- src/views/RVV1Vaults.vue
+++ src/views/RVV1Vaults.vue
@@ -1,140 +1,159 @@
 <template>
-	<VContainer>
-		<VRow>
-			<VCol cols="12">
-				<h2 class="text-center text-uppercase text-primary">V1 Vaults</h2>
-			</VCol>
+	<VRow>
+		<VCol cols="3" lg="2" xl="1" class="bg-primary">
 
-			<VCol cols="12">
-				<h3 class="mb-3 text-center text-uppercase">V1 Vault Memberships</h3>
+		</VCol>
 
-				<h5 class="text-center">{{ v1Vaults }}</h5>
+		<VCol cols="9" lg="10" xl="11" class="bg-white">
+			<VContainer>
+				<VRow>
+					<VCol cols="12">
+						<h3 class="mb-3 text-center text-uppercase">V1 Vault Memberships</h3>
 
-				<h6 class="text-center">Record Contract: {{ record }}</h6>
-			</VCol>
+						<h5 class="text-center">{{ v1Vaults }}</h5>
 
-			<VCol cols="12">
-				<h3 class="mb-3 text-center text-uppercase">Deploy a Vault</h3>
-			</VCol>
+						<h6 class="text-center">Record Contract: {{ record }}</h6>
+					</VCol>
 
-			<VCol cols="12" md="6">
-				<div class="mb-4 px-3 py-3 border">
-					<h5>Admins</h5>
+					<VCol cols="12">
+						<h3 class="mb-3 text-center text-uppercase">Deploy a Vault</h3>
+					</VCol>
 
-					<VRow
-						v-for="(m, i) in deployParams.admins" :key="i"
-						class="mb-3"
-					>
-						<VCol md="10">
-							<h5 class="my-2">{{ m }}</h5>
-						</VCol>
-						<VCol>
-							<VBtn
-								color="warning"
-								size="small"
-								class="w-100"
-								@click="removeAdmin(i)"
-							>✕</VBtn>
-						</VCol>
-					</VRow>
+					<!-- Sig Manager -->
+					<VCol cols="12">
+						<VTextField
+							v-model="deployParams.signatureManager"
+							type="text"
+							label="Signature Manager"
+							variant="outlined"
+							hide-details
+							class="mb-3"
+							size="small"
+						/>
+					</VCol>
 
-					<VRow>
-						<VCol md="10">
-							<VTextField v-model="addAdminField" label="Address" variant="outlined" />
-						</VCol>
-						<VCol>
-							<VBtn
-								color="success"
-								class="w-100 my-3"
-								size="small"
-								@click="addAdmin()"
-							>+</VBtn>
-						</VCol>
-					</VRow>
-				</div>
+					<!-- Against -->
+					<VCol md="4">
+						<VTextField
+							v-model="deployParams.againstVoteCountRequired"
+							type="number"
+							label="Against Vote Count"
+							variant="outlined"
+							hide-details
+							class="mb-3"
+						/>
+					</VCol>
 
-				<div class="mb-4 px-3 py-3 border">
-					<h5>Members</h5>
+					<!-- For -->
+					<VCol md="4">
+						<VTextField
+							v-model="deployParams.forVoteCountRequired"
+							type="number"
+							label="For Vote Count"
+							variant="outlined"
+							hide-details
+							class="mb-3"
+						/>
+					</VCol>
 
-					<VRow
-						v-for="(m, i) in deployParams.members" :key="i"
-						class="mb-3"
-					>
-						<VCol md="10">
-							<h5 class="my-2">{{ m }}</h5>
-						</VCol>
-						<VCol>
-							<VBtn
-								color="warning"
-								size="small"
-								class="w-100"
-								@click="removeMember(i)"
-							>✕</VBtn>
-						</VCol>
-					</VRow>
+					<!-- Withdrawal Delay -->
+					<VCol md="4">
+						<VTextField
+							v-model="deployParams.withdrawalDelaySeconds"
+							type="number"
+							label="Withdrawal Delay (Seconds)"
+							variant="outlined"
+							hide-details
+							class="mb-3"
+						/>
+					</VCol>
 
-					<VRow>
-						<VCol md="10">
-							<VTextField v-model="addMemberField" label="Address" variant="outlined" />
-						</VCol>
-						<VCol>
-							<VBtn
-								color="success"
-								class="w-100 my-3"
-								size="small"
-								@click="addMember()"
-							>+</VBtn>
-						</VCol>
-					</VRow>
-				</div>
-			</VCol>
+					<!-- Admins -->
+					<VCol md="6">
+						<div class="mb-4 px-3 py-3 border">
+							<h5>Admins</h5>
 
-			<VCol cols="12" md="6">
-				<VTextField
-					v-model="deployParams.signatureManager"
-					type="text"
-					label="Signature Manager"
-					variant="outlined"
-					hide-details
-					class="mb-3"
-					size="small"
-				/>
+							<VRow
+								v-for="(m, i) in deployParams.admins" :key="i"
+								class="mb-3"
+							>
+								<VCol md="10">
+									<h6 class="my-2">{{ m }}</h6>
+								</VCol>
+								<VCol md="2">
+									<VBtn
+										color="warning"
+										size="small"
+										class="w-100"
+										@click="removeAdmin(i)"
+									>✕</VBtn>
+								</VCol>
+							</VRow>
 
-				<VTextField
-					v-model="deployParams.againstVoteCountRequired"
-					type="number"
-					label="Against Vote Count"
-					variant="outlined"
-					hide-details
-					class="mb-3"
-				/>
+							<VRow>
+								<VCol md="10">
+									<VTextField v-model="addAdminField" label="Address" variant="outlined" />
+								</VCol>
+								<VCol md="2">
+									<VBtn
+										color="success"
+										class="w-100 my-3"
+										size="small"
+										@click="addAdmin()"
+									>+</VBtn>
+								</VCol>
+							</VRow>
+						</div>
+					</VCol>
 
-				<VTextField
-					v-model="deployParams.forVoteCountRequired"
-					type="number"
-					label="For Vote Count"
-					variant="outlined"
-					hide-details
-					class="mb-3"
-				/>
+					<!-- Members -->
+					<VCol md="6">
+						<div class="mb-4 px-3 py-3 border">
+							<h5>Members</h5>
 
-				<VTextField
-					v-model="deployParams.withdrawalDelaySeconds"
-					type="number"
-					label="Withdrawal Delay (Seconds)"
-					variant="outlined"
-					hide-details
-					class="mb-3"
-				/>
-			</VCol>
+							<VRow
+								v-for="(m, i) in deployParams.members" :key="i"
+								class="mb-3"
+							>
+								<VCol md="10">
+									<h6 class="my-2">{{ m }}</h6>
+								</VCol>
+								<VCol md="2">
+									<VBtn
+										color="warning"
+										size="small"
+										class="w-100"
+										@click="removeMember(i)"
+									>✕</VBtn>
+								</VCol>
+							</VRow>
 
-			<VCol cols="12">
-				<VBtn color="primary" class="w-100 mb-3" @click="deployYieldSyncV1Vault()">
-					Deploy
-				</VBtn>
-			</VCol>
-		</VRow>
-	</VContainer>
+							<VRow>
+								<VCol md="10">
+									<VTextField v-model="addMemberField" label="Address" variant="outlined" />
+								</VCol>
+								<VCol md="2">
+									<VBtn
+										color="success"
+										class="w-100 my-3"
+										size="small"
+										@click="addMember()"
+									>+</VBtn>
+								</VCol>
+							</VRow>
+						</div>
+					</VCol>
+
+					<VCol cols="12">
+						<h6>{{ deploymentFee }}</h6>
+						<VBtn color="primary" class="w-100 mb-3" @click="deployYieldSyncV1Vault()">
+							Deploy
+						</VBtn>
+					</VCol>
+				</VRow>
+			</VContainer>
+		</VCol>
+	</VRow>
 </template>
 
 <script lang="ts">
@@ -155,6 +174,8 @@
 				addAdminField: "",
 				addMemberField: "",
 
+				deploymentFee: 0,
+
 				deployParams: {
 					admins: [
 						this.$store.state.accounts[0],
@@ -164,9 +185,9 @@
 					] as string[],
 					signatureManager: ethers.ZeroAddress as string,
 					useDefaultSignatureManager: false as boolean,
-					againstVoteCountRequired: undefined as number | undefined,
-					forVoteCountRequired: undefined as number | undefined,
-					withdrawalDelaySeconds: undefined as number | undefined
+					againstVoteCountRequired: 1 as number | undefined,
+					forVoteCountRequired: 1 as number | undefined,
+					withdrawalDelaySeconds: 0 as number | undefined
 				},
 			};
 		},
@@ -237,6 +258,8 @@
 			this.v1Vaults = await this.$store.state.contract.yieldSyncV1VaultRecord.methods.member_yieldSyncV1Vaults(
 				this.$store.state.accounts[0]
 			).call();
+
+			this.deploymentFee = await this.$store.state.contract.yieldSyncV1VaultFactory.methods.fee().call();
 		},
 	});
 </script>
