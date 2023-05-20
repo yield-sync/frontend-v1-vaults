f251ecfd2976fc2d221c9622c4629bc3d6d8da39
harpoon js <w3st.io2021@gmail.com>
2023-05-20 19:05:12 -0400

    Better typing

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,33 @@
+type yieldSyncContractAddresses = {
+	yieldSyncGovernance: string,
+	yieldSyncV1VaultFactory: string,
+	yieldSyncV1VaultAccessControl: string
+}
+
+type networkYieldSyncContractAddresses = {
+	goerli: yieldSyncContractAddresses,
+	mainnet: yieldSyncContractAddresses,
+	sepolia: yieldSyncContractAddresses
+}
+
 export default {
 	address: {
 		goerli: {
 			yieldSyncGovernance: "",
 			yieldSyncV1VaultFactory: "",
-			yieldSyncV1VaultAccessControl: ""
+			yieldSyncV1VaultAccessControl: "",
 		},
 
 		mainnet: {
 			yieldSyncGovernance: "",
 			yieldSyncV1VaultFactory: "",
-			yieldSyncV1VaultAccessControl: ""
+			yieldSyncV1VaultAccessControl: "",
 		},
 
 		sepolia: {
 			yieldSyncGovernance: "0x2dbd8bA2F7C400f15d8779F4bB234f0CD60e4617",
 			yieldSyncV1VaultFactory: "0x2C86F1b70096311556994773E692a715e92638e5",
-			yieldSyncV1VaultAccessControl: "0x644E18912D49Cef9c7920B2fCDBD1b64c6CD17c5"
+			yieldSyncV1VaultAccessControl: "0x644E18912D49Cef9c7920B2fCDBD1b64c6CD17c5",
 		},
-	}
+	} as networkYieldSyncContractAddresses
 };
