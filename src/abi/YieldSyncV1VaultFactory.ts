70017938360f7d23233f38e6fa811301d8370830
harpoon js <w3st.io2021@gmail.com>
2023-06-11 21:25:17 -0400

    Fix formatting of JSON object

--- src/abi/YieldSyncV1VaultFactory.ts
+++ src/abi/YieldSyncV1VaultFactory.ts
@@ -2,106 +2,186 @@ export default [
 	{
 		"inputs": [
 			{
-				"internalType": "address", "name": "_yieldSyncV1Vault", "type": "address"
+				"internalType": "address",
+				"name": "_yieldSyncV1Vault",
+				"type": "address"
 			},
 			{
-				"internalType": "address", "name": "admin", "type": "address"
+				"internalType": "address",
+				"name": "admin",
+				"type": "address"
 			},
-		], "name": "addAdmin", "outputs": [
-		], "stateMutability": "nonpayable", "type": "function"
+		],
+		"name": "addAdmin",
+		"outputs": [
+
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
 	},
 	{
 		"inputs": [
 			{
-				"internalType": "address", "name": "_yieldSyncV1Vault", "type": "address"
+				"internalType": "address",
+				"name": "_yieldSyncV1Vault",
+				"type": "address"
 			},
 			{
-				"internalType": "address", "name": "member", "type": "address"
+				"internalType": "address",
+				"name": "member",
+				"type": "address"
 			},
-		], "name": "addMember", "outputs": [
-		], "stateMutability": "nonpayable", "type": "function"
+		],
+		"name": "addMember",
+		"outputs": [
+
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
 	},
 	{
 		"inputs": [
 			{
-				"internalType": "address", "name": "admin", "type": "address"
+				"internalType": "address",
+				"name": "admin",
+				"type": "address"
 			},
-		], "name": "admin_yieldSyncV1Vaults", "outputs": [
+		],
+		"name": "admin_yieldSyncV1Vaults",
+		"outputs": [
 			{
-				"internalType": "address[]", "name": "", "type": "address[]"
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
 			},
-		], "stateMutability": "view", "type": "function"
+		],
+		"stateMutability": "view",
+		"type": "function"
 	},
 	{
 		"inputs": [
 			{
-				"internalType": "address", "name": "member", "type": "address"
+				"internalType": "address",
+				"name": "member",
+				"type": "address"
 			},
-		], "name": "member_yieldSyncV1Vaults", "outputs": [
+		],
+		"name": "member_yieldSyncV1Vaults",
+		"outputs": [
 			{
-				"internalType": "address[]", "name": "", "type": "address[]"
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
 			},
-		], "stateMutability": "view", "type": "function"
+		],
+		"stateMutability": "view",
+		"type": "function"
 	},
 	{
 		"inputs": [
 			{
-				"internalType": "address", "name": "participant", "type": "address"
+				"internalType": "address",
+				"name": "participant",
+				"type": "address"
 			},
 			{
-				"internalType": "address", "name": "yieldSyncV1Vault", "type": "address"
+				"internalType": "address",
+				"name": "yieldSyncV1Vault",
+				"type": "address"
 			},
-		], "name": "participant_yieldSyncV1Vault_access", "outputs": [
+		],
+		"name": "participant_yieldSyncV1Vault_access",
+		"outputs": [
 			{
-				"internalType": "bool", "name": "admin", "type": "bool"
+				"internalType": "bool",
+				"name": "admin",
+				"type": "bool"
 			},
 			{
-				"internalType": "bool", "name": "member", "type": "bool"
+				"internalType": "bool",
+				"name": "member",
+				"type": "bool"
 			},
-		], "stateMutability": "view", "type": "function"
+		],
+		"stateMutability": "view",
+		"type": "function"
 	},
 	{
 		"inputs": [
 			{
-				"internalType": "address", "name": "_yieldSyncV1Vault", "type": "address"
+				"internalType": "address",
+				"name": "_yieldSyncV1Vault",
+				"type": "address"
 			},
 			{
-				"internalType": "address", "name": "admin", "type": "address"
+				"internalType": "address",
+				"name": "admin",
+				"type": "address"
 			},
-		], "name": "removeAdmin", "outputs": [
-		], "stateMutability": "nonpayable", "type": "function"
+		],
+		"name": "removeAdmin",
+		"outputs": [
+
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
 	},
 	{
 		"inputs": [
 			{
-				"internalType": "address", "name": "_yieldSyncV1Vault", "type": "address"
+				"internalType": "address",
+				"name": "_yieldSyncV1Vault",
+				"type": "address"
 			},
 			{
-				"internalType": "address", "name": "member", "type": "address"
+				"internalType": "address",
+				"name": "member",
+				"type": "address"
 			},
-		], "name": "removeMember", "outputs": [
-		], "stateMutability": "nonpayable", "type": "function"
+		],
+		"name": "removeMember",
+		"outputs": [
+
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
 	},
 	{
 		"inputs": [
 			{
-				"internalType": "address", "name": "yieldSyncV1Vault", "type": "address"
+				"internalType": "address",
+				"name": "yieldSyncV1Vault",
+				"type": "address"
 			},
-		], "name": "yieldSyncV1Vault_admins", "outputs": [
+		],
+		"name": "yieldSyncV1Vault_admins",
+		"outputs": [
 			{
-				"internalType": "address[]", "name": "", "type": "address[]"
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
 			},
-		], "stateMutability": "view", "type": "function"
+		],
+		"stateMutability": "view",
+		"type": "function"
 	},
 	{
 		"inputs": [
 			{
-				"internalType": "address", "name": "yieldSyncV1Vault", "type": "address"
+				"internalType": "address",
+				"name": "yieldSyncV1Vault",
+				"type": "address"
 			},
-		], "name": "yieldSyncV1Vault_members", "outputs": [
+		],
+		"name": "yieldSyncV1Vault_members",
+		"outputs": [
 			{
-				"internalType": "address[]", "name": "", "type": "address[]"
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
 			},
-		], "stateMutability": "view", "type": "function"
+		],
+		"stateMutability": "view",
+		"type": "function"
 	},
 ];
