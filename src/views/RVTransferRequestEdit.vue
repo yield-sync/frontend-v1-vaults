3f70c89562b2f6fea61e17c4d8653805a56146a3
harpoon js <w3st.io2021@gmail.com>
2023-06-11 21:33:54 -0400

    Progress towards allowing admin to edit transferRequest

--- src/views/RVTransferRequestEdit.vue
+++ src/views/RVTransferRequestEdit.vue
@@ -17,31 +17,6 @@
 </template>
 
 <script lang="ts">
-	type TransferRequest = [
-		// forERC20
-		boolean,
-		// forERC721
-		boolean,
-		// creator
-		string,
-		// token
-		string,
-		// tokenId
-		string,
-		// amount
-		string,
-		// to
-		string,
-		// forVoteCount
-		string,
-		// againstVoteCount
-		string,
-		// latestRelevantForVoteTime
-		string,
-		// votedMembers
-		string[],
-	];
-
 	import { defineComponent } from "vue";
 	import { Contract } from "web3-eth-contract";
 	import { AbiItem } from "web3-utils";
@@ -55,14 +30,37 @@
 		{
 			return {
 				yieldSyncV1Vault: undefined as undefined | Contract,
-				solTransferRequest: undefined as undefined | TransferRequest,
+				solTransferRequest: undefined as undefined | [
+					// 0 forERC20
+					boolean,
+					// 1 forERC721
+					boolean,
+					// 2 creator
+					string,
+					// 3 token
+					string,
+					// 4 tokenId
+					string,
+					// 5 amount
+					string,
+					// 6 to
+					string,
+					// 7 forVoteCount
+					string,
+					// 8 againstVoteCount
+					string,
+					// 9 latestRelevantForVoteTime
+					string,
+					// 10 votedMembers
+					string[],
+				],
 				transferRequest: {
 					for: "Ether" as "Ether" | "ERC20" | "ERC721",
 					creator: "" as string,
-					to: "" as string,
 					token: "" as string,
-					amount: 0 as number,
 					tokenId: 0 as number,
+					to: "" as string,
+					amount: 0 as number,
 				}
 			};
 		},
@@ -93,10 +91,10 @@
 					}
 
 					this.transferRequest.creator = String(this.solTransferRequest[2]);
-
-					this.transferRequest.to = String(this.solTransferRequest[3]);
-
-					this.transferRequest.token =  String(this.solTransferRequest[4]);
+					this.transferRequest.token =  String(this.solTransferRequest[3]);
+					this.transferRequest.tokenId =  parseInt(this.solTransferRequest[4]);
+					this.transferRequest.to = String(this.solTransferRequest[6]);
+					this.transferRequest.amount = parseInt(this.solTransferRequest[5]);
 				}
 			}
 		},
